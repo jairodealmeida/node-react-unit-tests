4674e3f9df6e266d6d04be4348782f9879cf3d4d
C1329797 Jairo De Almeida <c1329797@interno.bb.com.br>
2024-12-06 18:56:30 -0300

    Aula 01 teste elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App2';
 import reportWebVitals from './reportWebVitals';
+import LoginComponent from './LoginComponent';
+import LoginService from './services/LoginService';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const loginService = new LoginService();
+
+const setToken = (token: string) =>{
+  console.log(`received the token ${token}`);
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <LoginComponent loginService={loginService} setToken={setToken} /> 
 );
 
 // If you want to start measuring performance in your app, pass a function
